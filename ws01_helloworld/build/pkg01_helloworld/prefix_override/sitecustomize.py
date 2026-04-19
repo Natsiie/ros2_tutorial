b4ca6bc5de1ff39b1b4a705807e7a9475f7b0de9
Natsiie <nate_0228@qq.com>
2026-04-19 16:32:16 +0800

    全部提交

--- ws01_helloworld/build/pkg01_helloworld/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/lulu/ros2_tutorial/ws01_helloworld/install/pkg01_helloworld'
--- ws01_helloworld/build/pkg01_helloworld/share/pkg01_helloworld/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/lulu/ros2_tutorial/ws01_helloworld/build/pkg01_helloworld"
